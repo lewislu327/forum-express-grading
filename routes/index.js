5fb143c52f2041bf64b6b5802d9982fba7eb62c4
Lewis Lu <agooday32@gmail.com>
2021-06-16 22:06:26 +0800

    Add authentication before rendering pages

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,34 @@ const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController')
 
 module.exports = (app, passport) => {
- app.get('/', (req, res) => res.redirect('/restaurants'))
- app.get('/restaurants', restController.getRestaurants)
+  const authenticated = (req, res , next) => {
+    if (req.isAuthenticated()){
+      return next()
+    }
+    res.redirect('/signin')
+  }
 
- app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
- app.get('/admin/restaurants', adminController.getRestaurants)
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()){
+      if (req.isAuthenticated()){
+        if (req.user.isAdmin) {
+          return next()
+        }
+      }
+      res.redirect('/signin')
+    }
+  }
+  
+  app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
+  app.get('/restaurants', authenticated, restController.getRestaurants)
 
- app.get('/signup', userController.signUpPage)
- app.post('/signup', userController.signUp)
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
 
- app.get('/signin', userController.signInPage)
- app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
- app.get('/logout', userController.logout)
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
+
+  app.get('/signin', userController.signInPage)
+  app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+  app.get('/logout', userController.logout)
 }
