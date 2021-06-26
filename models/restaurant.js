8ec193443ba49e1daef1430c7019e0fae0210d15
lewislu327 <agooday32@gmail.com>
2021-06-26 11:53:46 +0800

    Add favorite/unfavorite feature

--- models/restaurant.js
+++ models/restaurant.js
@@ -11,7 +11,12 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       Restaurant.belongsTo(models.Category)
-      Restaurant.hasMany(models.Comment)
+      Restaurant.hasMany(models.Comment),
+       Restaurant.belongsToMany(models.User, {
+      through: models.Favorite,
+      foreignKey: 'RestaurantId',
+      as: 'FavoritedUsers'
+    })
     }
   };
   Restaurant.init({
