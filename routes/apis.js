b24d13dd777d30a8023f4a3ad6d66c81b946f832
lewislu327 <agooday32@gmail.com>
2021-06-30 22:32:43 +0800

    add POST /api/signin (return jwt) & rename controllers/api folders

--- routes/apis.js
+++ routes/apis.js
@@ -3,8 +3,11 @@ const router = express.Router()
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
 
-const adminController = require('../controllers/apis/adminController.js')
-const categoryController = require('../controllers/apis/categoryController')
+const adminController = require('../controllers/api/adminController.js')
+const categoryController = require('../controllers/api/categoryController')
+const userController = require('../controllers/api/userController')
+
+router.post('/signin', userController.signIn)
 
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
