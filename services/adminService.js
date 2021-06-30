d451f958bd72f0bf094989ed29805464c10d7746
lewislu327 <agooday32@gmail.com>
2021-06-30 15:09:28 +0800

    POST /api/admin/restaurants

--- services/adminService.js
+++ services/adminService.js
@@ -16,12 +16,12 @@ const adminService = {
     .then(restaurants => { callback({ restaurants: restaurants })})
   },
 
-   getRestaurant: (req, res, callback) => {
+  getRestaurant: (req, res, callback) => {
     return Restaurant.findByPk(req.params.id, { include: [Category] })
       .then(restaurant => { callback ({ restaurant: restaurant.toJSON() })})
   },
 
-    deleteRestaurant: (req, res, callback) => {
+  deleteRestaurant: (req, res, callback) => {
     return Restaurant.findByPk(req.params.id)
       .then((restaurant) => {
         restaurant.destroy()
@@ -29,7 +29,43 @@ const adminService = {
             callback({ status: 'success', message: '' })
           })
       })
-  }
+  },
+
+  postRestaurant: (req, res, callback) => {
+    if (!req.body.name) {
+      return callback({  status: 'error', message: "name didn't exist" })
+    }
+    const { file } = req // equal to const file = req.file
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID);
+      imgur.upload(file.path, (err, img) => {
+        return Restaurant.create({
+          name: req.body.name,
+          tel: req.body.tel,
+          address: req.body.address,
+          opening_hours: req.body.opening_hours,
+          description: req.body.description,
+          image: file ? img.data.link : null,
+          CategoryId: req.body.categoryId
+        })
+        .then((restaurant) => {
+          return callback({  status: 'success', message: "restaurant was successfully created" })
+        })
+      })
+    } else {
+      return Restaurant.create({
+        name: req.body.name,
+        tel: req.body.tel,
+        address: req.body.address,
+        opening_hours: req.body.opening_hours,
+        description: req.body.description,
+        image: null,
+        CategoryId: req.body.categoryId
+      }).then((restaurant) => {
+        callback({ status: 'success', message: 'restaurant was successfully created' })
+      })
+    }
+  },
 
 }
 
