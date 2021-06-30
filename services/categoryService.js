24d35dcac7f465ea4840fb36979bec67910c8e80
lewislu327 <agooday32@gmail.com>
2021-06-30 16:25:20 +0800

    PUT /api/admin/categories/:id

--- services/categoryService.js
+++ services/categoryService.js
@@ -27,6 +27,20 @@ const categoryService = {
       .then( category => { callback({ status: 'success',  message: "category was successfully created" }) })
     }
   },
+
+  putCategory: (req, res, callback) => {
+    if (!req.body.name) {
+      callback({ status: 'error', message: 'name didn\'t exist' })
+    } else {
+      return Category.findByPk(req.params.id)
+        .then((category) => {
+          category.update(req.body)
+            .then((category) => {
+              callback({ status: 'success', message: 'category was successfully updated' })
+            })
+        })
+    }
+  },
 }
 
 module.exports = categoryService
