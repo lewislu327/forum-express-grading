9193251eb31b39a16dd171e76a781ff5bba58704
lewislu327 <agooday32@gmail.com>
2021-06-30 16:33:20 +0800

    DELETE /api/admin/categories/:id

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -33,13 +33,12 @@ let categoryController = {
   },
 
   deleteCategory: (req, res) => {
-    return Category.findByPk(req.params.id)
-      .then( category =>  { 
-        category.destroy()
-          .then(category => {
-            res.redirect('/admin/categories')
-          })
-      })
+    categoryService.deleteCategory(req, res, (data) => {
+      if (data.status === 'success') {
+        req.flash('success_messages', data['message'])
+        res.redirect('/admin/categories')
+      }
+    })
   }
 }
 
